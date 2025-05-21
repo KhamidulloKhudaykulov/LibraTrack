53414166b37e5390871759bec068f26ca78f417b
Khamidullo Khudaykulov <xudaykulov123@gmail.com>
2025-05-21 23:11:21 +0500

    Added ItemCreatedDomainEvent

---
+++ src/Services/InventoryService/InventoryService.Domain/Events/Items/ItemCreatedDomainEvent.cs
@@ -0,0 +1,14 @@
+﻿using InventoryService.Domain.Primitives;
+using MediatR;
+
+namespace InventoryService.Domain.Events.Items;
+
+public class ItemCreatedDomainEvent : DomainEvent, INotification
+{
+    public Guid ProductId { get; set; }
+
+    public ItemCreatedDomainEvent(Guid productId)
+    {
+        ProductId = productId;
+    }
+}
