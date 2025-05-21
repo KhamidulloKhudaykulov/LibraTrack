ad4e29143ae8535cacb439d7a47c724e0a5c15b2
Khamidullo Khudaykulov <xudaykulov123@gmail.com>
2025-05-21 23:41:11 +0500

    DI added

---
+++ src/Services/InventoryService/InventoryService.Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using InventoryService.Application.Interfaces.Clients;
+using Microsoft.Extensions.DependencyInjection;
+using NotificationService.Infrastructure.Grpc.Clients;
+using Grpc.Net.Client;
+
+namespace InventoryService.Infrastructure.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<IBookServiceClient, BookServiceClient>();
+
+        return services;
+    }
+}
