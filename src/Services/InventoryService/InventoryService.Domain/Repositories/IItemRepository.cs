12f88186fd959b95b6bdab65d8070e06075574de
Khamidullo Khudaykulov <xudaykulov123@gmail.com>
2025-05-21 22:08:40 +0500

    Created IItemRepository

---
+++ src/Services/InventoryService/InventoryService.Domain/Repositories/IItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using InventoryService.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace InventoryService.Domain.Repositories;
+
+public interface IItemRepository
+{
+    Task<Item> InsertAsync(Item item);
+    Task<Item> UpdateAsync(Item item);
+    Task DeleteAsync(Item item);
+    Task<Item> SelectAsync(Expression<Func<Item, bool>> expression);
+    Task<IEnumerable<Item>> SelectAllAsync(Expression<Func<Item, bool>>? expression = null);
+}
