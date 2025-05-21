da196e1346ee336d76f3d39d59fd577c4e5425ae
Khamidullo Khudaykulov <xudaykulov123@gmail.com>
2025-05-21 22:19:21 +0500

    The persistence layer has been developed.

---
+++ src/Services/InventoryService/InventoryService.Persistence/Configurations/ItemConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using InventoryService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InventoryService.Persistence.Configurations;
+
+public class ItemConfiguration : IEntityTypeConfiguration<Item>
+{
+    public void Configure(EntityTypeBuilder<Item> builder)
+    {
+        builder.ToTable("ItemStock");
+
+        builder.Property(i => i.Price)
+            .IsRequired(true);
+    }
+}
