0f2025afb6908ab17896b82fb532654995d3f48f
Khamidullo Khudaykulov <xudaykulov123@gmail.com>
2025-05-24 07:40:18 +0500

    Added ApplicationDbContext to DI

--- src/Services/InventoryService/InventoryService.Persistence/Extensions/DependencyInjection.cs
+++ src/Services/InventoryService/InventoryService.Persistence/Extensions/DependencyInjection.cs
@@ -1,15 +1,24 @@
 ﻿using InventoryService.Domain.Repositories;
 using InventoryService.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace InventoryService.Persistence.Extensions;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddPersistence(this IServiceCollection services)
+    public static IServiceCollection AddPersistence(
+        this IServiceCollection services,
+        IConfiguration configuration)
     {
         services.AddScoped<IItemRepository, ItemRepository>();
         services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("Default"));
+        });
 
         return services;
     }
