8f2dbad2ab575d70b9d879b470e7914ecbcb8771
Khamidullo Khudaykulov <xudaykulov123@gmail.com>
2025-05-24 08:00:42 +0500

    fixed bugs

--- src/Services/InventoryService/InventoryService.Application/UseCases/Items/Queries/GetAvailableItemQuantityQuery.cs
+++ src/Services/InventoryService/InventoryService.Application/UseCases/Items/Queries/GetAvailableItemQuantityQuery.cs
@@ -6,19 +6,19 @@ using InventoryService.Domain.Shared;
 namespace InventoryService.Application.UseCases.Items.Queries;
 
 public record GetAvailableItemQuantityQuery(
-    Guid itemId) : IQuery<int>;
+    Guid productId) : IQuery<int>;
 
 public class GetAvailableItemQuantityQueryHandler(
     IItemRepository _itemRepository) : IQueryHandler<GetAvailableItemQuantityQuery, int>
 {
     public async Task<Result<int>> Handle(GetAvailableItemQuantityQuery request, CancellationToken cancellationToken)
     {
-        var item = await _itemRepository.SelectAsync(i => i.Id == request.itemId);
+        var item = await _itemRepository.SelectAsync(i => i.ProductId == request.productId);
         if (item is null)
         {
             return Result.Failure<int>(new Error(
                 code: "Item.NotFound",
-                message: $"This item with ID={request.itemId} is not found"));
+                message: $"This item with ID={request.productId} is not found"));
         }
 
         return Result.Success(item.Amount);
