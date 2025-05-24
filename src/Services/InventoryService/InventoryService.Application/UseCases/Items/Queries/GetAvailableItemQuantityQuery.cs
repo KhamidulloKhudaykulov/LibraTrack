83f09cf2bed023989daa028c6d31d11f2ef71599
Khamidullo Khudaykulov <xudaykulov123@gmail.com>
2025-05-24 07:30:20 +0500

    Created Get Availbale Quantity of Item Query

---
+++ src/Services/InventoryService/InventoryService.Application/UseCases/Items/Queries/GetAvailableItemQuantityQuery.cs
@@ -0,0 +1,26 @@
+﻿using InventoryService.Application.Abstractions.Messaging;
+using InventoryService.Domain.Entities;
+using InventoryService.Domain.Repositories;
+using InventoryService.Domain.Shared;
+
+namespace InventoryService.Application.UseCases.Items.Queries;
+
+public record GetAvailableItemQuantityQuery(
+    Guid itemId) : IQuery<int>;
+
+public class GetAvailableItemQuantityQueryHandler(
+    IItemRepository _itemRepository) : IQueryHandler<GetAvailableItemQuantityQuery, int>
+{
+    public async Task<Result<int>> Handle(GetAvailableItemQuantityQuery request, CancellationToken cancellationToken)
+    {
+        var item = await _itemRepository.SelectAsync(i => i.Id == request.itemId);
+        if (item is null)
+        {
+            return Result.Failure<int>(new Error(
+                code: "Item.NotFound",
+                message: $"This item with ID={request.itemId} is not found"));
+        }
+
+        return Result.Success(item.Amount);
+    }
+}
