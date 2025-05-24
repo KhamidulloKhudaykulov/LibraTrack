d222f6a700a8027c2bf7f4bf32ae0afabcfe214e
Khamidullo Khudaykulov <xudaykulov123@gmail.com>
2025-05-24 07:36:18 +0500

    Created InventoryContoller and integrated http methods

---
+++ src/Services/InventoryService/InventoryService.Api/Contollers/InventoryControllers.cs
@@ -0,0 +1,54 @@
+﻿using InventoryService.Application.UseCases.Items.Commands;
+using InventoryService.Application.UseCases.Items.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryService.Api.Contollers;
+
+[ApiController]
+[Route("api/inventory")]
+public class InventoryControllers : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public InventoryControllers(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ReceiveItem([FromBody]ReceiveStockCommand command)
+    {
+        var response = await _sender.Send(command);
+        if (response.IsFailure)
+        {
+            return BadRequest(response.Error);
+        }
+
+        return Ok(response);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> ReceiveItem([FromBody]DeductItemFromStockCommand command)
+    {
+        var response = await _sender.Send(command);
+        if (response.IsFailure)
+        {
+            return BadRequest(response.Error);
+        }
+
+        return Ok(response);
+    }
+
+    [HttpGet("quantity")]
+    public async Task<IActionResult> GetQuantity([FromQuery]GetAvailableItemQuantityQuery query)
+    {
+        var response = await _sender.Send(query);
+        if (response.IsFailure)
+        {
+            return BadRequest(response.Error);
+        }
+
+        return Ok(response);
+    }
+}
