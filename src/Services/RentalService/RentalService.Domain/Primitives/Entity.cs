14c6518b63d1b1bfad07652a596db7cd87521054
Khamidullo Khudaykulov <xudaykulov123@gmail.com>
2025-05-25 17:21:22 +0500

    Added CreatedAt And IsDeleted Properties

--- src/Services/RentalService/RentalService.Domain/Primitives/Entity.cs
+++ src/Services/RentalService/RentalService.Domain/Primitives/Entity.cs
@@ -3,10 +3,18 @@
 public abstract class Entity
 {
     public Guid Id { get; protected set; }
+    public DateTime CreatedAt { get; protected set; }
+    public bool IsDeleted { get; protected set; } = false;
     public Entity()
-        => Id = Guid.NewGuid();
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+    }
     public Entity(Guid id)
-        => Id = id;
+    {
+        Id = id;
+        CreatedAt = DateTime.UtcNow;
+    }
 
     private List<IDomainEvent> _domainEvents = new();
     public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
