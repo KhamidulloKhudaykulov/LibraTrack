5e12d9c78250cc1dcf2c7295eb4b291c54b74538
Khamidullo Khudaykulov <xudaykulov123@gmail.com>
2025-05-25 17:18:40 +0500

    Integrated Drop method and Modified Delete method

--- src/Services/RentalService/RentalService.Domain/Repositories/IRentalRecordRepository.cs
+++ src/Services/RentalService/RentalService.Domain/Repositories/IRentalRecordRepository.cs
@@ -8,6 +8,7 @@ public interface IRentalRecordRepository
     Task<RentalRecord> InsertAsync(RentalRecord record);
     Task<RentalRecord> UpdateAsync(RentalRecord record);
     Task DeleteAsync(RentalRecord rentalRecord);
+    Task DropAsync(RentalRecord rentalRecord);
     Task<RentalRecord> SelectAsync(Expression<Func<RentalRecord, bool>> expression);
     Task<IEnumerable<RentalRecord>> SelectAllAsync(Expression<Func<RentalRecord, bool>>? expression = null);
     Task<IQueryable<RentalRecord>> SelectAllAsQueryableAsync(Expression<Func<RentalRecord, bool>>? expression = null);
