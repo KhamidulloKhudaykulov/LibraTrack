5dc48ad142fcb03bcb581f0d701a6ef5f6ad6555
Khamidullo Khudaykulov <xudaykulov123@gmail.com>
2025-05-29 18:31:18 +0500

    Created Contract Model to change data from api

---
+++ src/Services/InventoryService/InventoryService.Application/UseCases/Items/Contracts/GetItemsResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace InventoryService.Application.UseCases.Items.Contracts;
+
+public class GetItemsResponse
+{
+    public Guid Id { get; set; }
+    public string? ProductName { get; set; }
+    public int Amount { get; set; }
+    public int AvailableQuantity { get; set; }
+    public decimal Price { get; set; }
+    public decimal TotalPrice { get; set; }
+    public string? CreatedDate { get; set; }
+}
