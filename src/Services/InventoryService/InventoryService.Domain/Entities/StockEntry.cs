04219e94ec2e915b845f000b2168938d434f5328
Khamidullo Khudaykulov <xudaykulov123@gmail.com>
2025-05-30 20:32:15 +0500

    Item Entity Changed To StockEntry.cs And Created Warehouse Entity

--- src/Services/InventoryService/InventoryService.Domain/Entities/StockEntry.cs
+++ src/Services/InventoryService/InventoryService.Domain/Entities/StockEntry.cs
@@ -4,28 +4,25 @@ using InventoryService.Domain.Shared;
 
 namespace InventoryService.Domain.Entities;
 
-public class Item : Entity
+public class StockEntry : Entity
 {
-    protected Item(
+    protected StockEntry(
         Guid productId,
         int amount,
-        int availableQuantity,
         decimal price)
     {
         ProductId = productId;
         Amount = amount;
-        AvailableQuantity = availableQuantity;
         Price = price;
     }
 
     public Guid ProductId { get; private set; }
     public int Amount { get; private set; }
-    public int AvailableQuantity { get; private set; }
     public decimal Price { get; private set; }
     public decimal TotalPrice  => Amount * Price;
     public DateTime CreatedAt { get; } = DateTime.UtcNow;
 
-    public static Result<Item> Create(
+    public static Result<StockEntry> Create(
         Guid productId,
         int amount,
         int availableQuantity,
@@ -33,24 +30,9 @@ public class Item : Entity
         )
     {
         
-        var item = new Item(productId, amount, availableQuantity, price);
+        var item = new StockEntry(productId, amount, price);
         item.AddDomainEvent(new ItemCreatedDomainEvent(productId));
 
         return item;
-    }
-
-    public void AddAmount(int amount)
-    {
-        Amount += amount;
-    }
-
-    public void AddAvailableQuantity(int quantity)
-    {
-        AvailableQuantity += quantity;
-    }
-
-    public int RemoveAvailableQuantity(int quantity)
-    {
-        return AvailableQuantity -= quantity;
     }
 }
---
+++ src/Services/InventoryService/InventoryService.Domain/Entities/Warehouse.cs
@@ -0,0 +1,31 @@
+﻿using InventoryService.Domain.Primitives;
+using InventoryService.Domain.Shared;
+
+namespace InventoryService.Domain.Entities;
+
+public class Warehouse : Entity
+{
+    private Warehouse(Guid productId, int availableQuantity)
+    {
+        ProductId = productId;
+        AvailableQuantity = availableQuantity;
+    }
+
+    public Guid ProductId { get; set; }
+    public int AvailableQuantity { get; set; }
+
+    public Result<Warehouse> Create(Guid productId, int availableQuantity)
+    {
+        return new Warehouse(productId, availableQuantity);
+    }
+
+    public void AddQuantity(int quantity)
+    {
+        AvailableQuantity += quantity;
+    }
+
+    public void RemoveQuantity(int quantity)
+    {
+        AvailableQuantity -= quantity;
+    }
+}
